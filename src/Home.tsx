7e6afd8893f1d1cab1158c98b1078fe51c4b78fb
James Boyle <98388371+James-P-Boyle@users.noreply.github.com>
2023-07-17 13:47:16 +0200

    Add shape animations to home

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,13 +4,30 @@ export default function Home(){
 
 
   return (
-    <div>
-      <form>
-        <input type="text" placeholder="title" />
-      </form>
+    <>
+    <div id="home" className="arrow arrow--top">
+      <svg xmlns="http://www.w3.org/2000/svg" className="dotted-circle" width="352" height="352" overflow="visible">
+        <circle cx="176" cy="176" r="174" fill="none" stroke="#fff" stroke-width="2" stroke-miterlimit="10" stroke-dasharray="12.921,11.9271"/>
+      </svg>
+
+
+      <svg fill="none" stroke="#fff" strokeWidth={2} width="270.11" height="649.9" overflow="visible">
+        <g className="item-to bounce-1">
+          <path className="geo-arrow draw-in" d="M135.06 142.564L267.995 275.5 135.06 408.434 2.125 275.499z" />
+        </g>
+        <circle className="geo-arrow item-to bounce-2" cx="194.65" cy="69.54" r="11.96" />
+
+        <g className="geo-arrow item-to bounce-2">
+          <path className="st0 draw-in" d="M181.21 619.5l13.27 27 13.27-27zM194.48 644.5v-552" />
+        </g>
+
+        <circle className="geo-arrow item-to bounce-2" cx="194.65" cy="590.54" r="11.96" />
+      </svg>
 
 
     </div>
+    </>
   )
 
 }
+
